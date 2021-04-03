23896a55d096749b0d181343adb445005be539ea
Derek_Cabrera <cabrera.derek.adriel@gmail.com>
2021-04-03 09:17:27 -0300

    implementando js

--- js/archivo.js
+++ js/archivo.js
@@ -1,3 +1,16 @@
 'use strict'
+
+
+
+/*
 let logeo = document.querySelector("#logeo");
 
+function abrirLogeo(){
+  console.log('presion');
+ if(){
+   botones.style.height = '180px';
+   botones.style.widh = '260px'
+   botones.style.background = '#fff';
+ }
+    return true;
+  };*/
